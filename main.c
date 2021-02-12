cb812e2a62703a9a5c79ea2e0d4e547ae083c5c7
Saif Bashar <saifbashar2021@gmail.com>
2021-02-12 22:57:06 +0600

    8 number problem in string is added

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
-#include<math.h>
+#include<string.h>
 int main()
 {
-	int i,j;
-	for(i=1;i<=5;i++)
+	char a[100],b[100];
+	scanf("%s",a);
+	scanf("%s",b);
+	printf("%s %s",a,b);
+	if(strcmp(a,b) == 0)
 	{
-		for(j=1;j<=i;j++)
-		{
-			printf("%d ",j);
-		}
-		printf("\n");
-	}
+	  printf("They are equal\n");	
+	} 
+	if(strlen(a)>strlen(b)) printf("\nFirst is greater than Second one");
+	else printf("First is less than second one");
 	return 0;
 }
