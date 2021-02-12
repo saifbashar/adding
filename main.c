927591584053dc6058f3f13973f452f944f2cd26
Saif Bashar <saifbashar2021@gmail.com>
2021-02-12 16:08:44 +0600

    43 number problem added

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
-//Calculate grade
 #include<stdio.h>
 int main()
 {
-    int x = 5, y = 5;
-    int z;
-    z = x++;
-    z = ++y;
-    printf("%d", z);
-    return 0;
+	int i=1,sum = 0;
+	while(i<=20)
+	{
+		if(i%2==0)
+		{
+			sum += i;
+		}
+		i++;
+	}
+	printf("The sum of all even numbers between 1 to 20 is %d",sum);
+	return 0;
 }
